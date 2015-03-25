5d7c6aa43b37aeb98ca05fbbd5b48d7400662672
Robbert Harms <robbert@xkls.nl>
2015-03-25 10:59:39 +0100

    Updates to making docs

--- {{cookiecutter.repo_name}}/docs/conf.py
+++ {{cookiecutter.repo_name}}/docs/conf.py
@@ -40,7 +40,7 @@ import {{ cookiecutter.repo_name }}
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -273,3 +273,7 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+
+# -- Options for napoleon ----
+autoclass_content = 'both'
--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -5,47 +5,47 @@ import re
 from setuptools import setup, find_packages
 
 def load_requirements(fname):
-    is_comment = re.compile('^\s*(#|--).*').match
+    is_comment = re.compile("^\s*(#|--).*").match
     with open(fname) as fo:
         return [line.strip() for line in fo if not is_comment(line) and line.strip()]
 
-with open('README.rst', 'rt') as f: readme = f.read()
-with open('HISTORY.rst', 'rt') as f: history = f.read().replace('.. :changelog:', '')
-with open('{{ cookiecutter.repo_name }}/__init__.py') as f: version_file_contents = f.read()
+with open("README.rst", "rt") as f: readme = f.read()
+with open("HISTORY.rst", "rt") as f: history = f.read().replace(".. :changelog:", "")
+with open("{{ cookiecutter.repo_name }}/__init__.py") as f: version_file_contents = f.read()
 
-requirements = load_requirements('requirements.txt')
-requirements_tests = load_requirements('requirements_tests.txt')
+requirements = load_requirements("requirements.txt")
+requirements_tests = load_requirements("requirements_tests.txt")
 
 ver_dic = {}
-exec(compile(version_file_contents, "{{ cookiecutter.repo_name }}/__init__.py", 'exec'), ver_dic)
+exec(compile(version_file_contents, "{{ cookiecutter.repo_name }}/__init__.py", "exec"), ver_dic)
 
 setup(
-    name='{{ cookiecutter.repo_name }}',
+    name="{{ cookiecutter.repo_name }}",
     version=ver_dic["VERSION"],
-    description='{{ cookiecutter.project_short_description }}',
-    long_description=readme + '\n\n' + history,
-    author='{{ cookiecutter.full_name }}',
-    author_email='{{ cookiecutter.email }}',
-    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
+    description="{{ cookiecutter.project_short_description }}",
+    long_description=readme + "\n\n" + history,
+    author="{{ cookiecutter.full_name }}",
+    author_email="{{ cookiecutter.email }}",
+    url="https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}",
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
     license="LGPL v3",
     zip_safe=False,
-    keywords='{{ cookiecutter.repo_name }}',
+    keywords="{{ cookiecutter.repo_name }}",
     classifiers=[
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Natural Language :: English',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Topic :: Scientific/Engineering'
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Scientific/Engineering"
     ],
-    test_suite='tests',
+    test_suite="tests",
     tests_require=requirements_tests
 )
