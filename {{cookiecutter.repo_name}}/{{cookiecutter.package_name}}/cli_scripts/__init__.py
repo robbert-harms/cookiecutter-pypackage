ca2f940c4a75aa21169ed65dde2eb4fc2381ebb9
Robbert Harms <robbert@xkls.nl>
2017-02-23 08:41:10 +0100

    Adds __version__.py and cli_scripts dir

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/cli_scripts/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/cli_scripts/__init__.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-
 __author__ = '{{ cookiecutter.full_name }}'
 __date__ = '{{ cookiecutter.release_date }}'
 __email__ = '{{ cookiecutter.email }}'
 __license__ = "LGPL v3"
 __maintainer__ = "{{ cookiecutter.full_name }}"
-
-from .__version__ import VERSION, VERSION_STATUS, __version__
